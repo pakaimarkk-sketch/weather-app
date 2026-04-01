9c19704b4dc59bcfbb59b92686e763bd5b3e7984
pakaimarkk <251377257+pakaimarkk-sketch@users.noreply.github.com>
2026-04-01 21:28:34 +0200

    add init

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import "./styles.css";
-
-import { getWeather } from "./api";
+import { createWeatherLayout, createSearchLayout } from "./layout";
 import { renderWeather } from "./renderer";
+import { getWeather } from "./api";
+import { loadCurrentCity } from "./storage";
+import { initSearch } from "./searchController";
 
 async function init() {
-  const weather = await getWeather();
-  renderWeather(weather);
+  const app = document.querySelector("#weather");
+  const currentCity = loadCurrentCity();
+
+  if (!currentCity) {
+    app.append(createSearchLayout());
+    initSearch();
+  } else {
+    app.append(createWeatherLayout());
+    const weatherData = await getWeather(currentCity);
+    renderWeather(weatherData);
+  }
 }
 
 init();
